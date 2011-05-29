46c89991aecde42a8a51b3ead4ee5ef9219d9a87
booo <borgers@mi.fu-berlin.de>
2011-05-29 21:15:10 +0200

    support the node table

--- copy-importer.c
+++ copy-importer.c
@@ -1,4 +1,3 @@
-
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,28 +17,68 @@ int main(int argc, char ** argv) {
         PQfinish(conn);
         exit(1);
     }
-    res = PQexec(conn, "BEGIN");
+    res = PQexec(conn, "BEGIN;");
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
         fprintf(stderr, "COMMAND BEGIN failded: %s", PQerrorMessage(conn));
         PQclear(res);
         PQfinish(conn);
         exit(1);
     }
-    while(fgets(buf,1024,stdin)) {
-        printf("%s",buf);
+    res = PQexec(conn, "COPY nodes (id, version, user_id, tstamp, changeset_id, tags, geom) FROM STDIN DELIMITER ';'");
+    if (PQresultStatus(res) != PGRES_COPY_IN) {
+        fprintf(stderr, "COMMAND COPY FROM STDIN failed: %s", PQerrorMessage(conn));
         PQclear(res);
-        PQputCopyData(conn, buf, strlen(buf));
-        if(PQresultStatus(res) != PGRES_COMMAND_OK) {
-            fprintf(stderr, "COMMAND COPY failded: %s", PQerrorMessage(conn));
+        PQfinish(conn);
+        exit(1);
+    }
+    while(fgets(buf,1024,stdin)) {
+        //PQclear(res);
+        int success = PQputCopyData(conn, buf, strlen(buf));
+        if(success == 1)
+            printf("%s",buf);
+        /*if(PQresultStatus(res) != PGRES_COMMAND_OK) {
+            fprintf(stderr, "COMMAND COPY failed: %s", PQerrorMessage(conn));
             PQclear(res);
             PQfinish(conn);
             exit(1);
-        }
+        }*/
     }
-    int stop = PQputCopyEnd(conn, NULL);
-    if(stop != 1) {
-            fprintf(stderr, "COMMAND COPY failded: %s", PQerrorMessage(conn));
+    /*if(!PQgetResult(conn)) {
+        fprintf(stderr, "COMMAND COPY END failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        PQfinish(conn);
+        exit(1);
+    }*/
+    if(PQputCopyEnd(conn, NULL)) {
+        printf("termination data send\n");
+    }
+    while((res = PQgetResult(conn)) != NULL) {
+        printf("waiting...\n");
+    }
+    if(res != NULL) {
+        fprintf(stderr, "COMMAND PUT COPY END failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        PQfinish(conn);
+        exit(1);
+    }
+    PQendcopy(conn);
+    while((res = PQgetResult(conn)) != NULL) {
+        printf("waiting too...\n");
+    }
+    /*if(PQgetResult(conn)) {
+        fprintf(stderr, "COMMAND COPY END failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        PQfinish(conn);
+        exit(1);
+    }*/
+    res = PQexec(conn, "END;");
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        fprintf(stderr, "COMMAND END failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        PQfinish(conn);
+        exit(1);
     }
 
-
+    PQfinish(conn);
+    return EXIT_SUCCESS;
 }
